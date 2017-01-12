12d25e46f3c56066d0bbb19c63fc22cc46aa9228
Simone <simo.pelo94@gmail.com>
2017-01-12 23:00:10 +0100

    Controller stores creato, fixati alcuni bug. 	new file:   app/assets/javascripts/stores.coffee 	new file:   app/assets/stylesheets/stores.scss 	new file:   app/controllers/stores_controller.rb 	new file:   app/controllers/stores_controller.rb~ 	new file:   app/helpers/stores_helper.rb 	modified:   app/views/layouts/application.html.erb 	new file:   app/views/layouts/application.html.erb~ 	new file:   app/views/stores/edit.html.erb 	new file:   app/views/stores/edit.html.erb~ 	new file:   app/views/stores/index.html.erb 	new file:   app/views/stores/index.html.erb~ 	new file:   app/views/stores/new.html.erb 	new file:   app/views/stores/new.html.erb~ 	new file:   app/views/stores/show.html.erb 	new file:   app/views/stores/show.html.erb~ 	modified:   app/views/users/home.html.erb 	new file:   app/views/users/home.html.erb~ 	modified:   config/routes.rb 	new file:   config/routes.rb~ 	new file:   spec/controllers/stores_controller_spec.rb 	new file:   spec/helpers/stores_helper_spec.rb

---
+++ Social_Flayer/app/controllers/stores_controller.rb
@@ -0,0 +1,54 @@
+class StoresController < ApplicationController
+before_action :store, only: [:show, :edit, :update,:destroy]
+  
+  def show
+    @show=@store
+  end
+
+  def new
+    @store=Store.new
+  end
+
+  def create
+    @store=Store.new(store_params)
+    puts current_user.id
+    @store.owner_id=current_user.id
+    
+    if @store.save
+      redirect_to store_path(@store)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @store.update(store_params)
+      redirect_to store_path(@store)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @store.destroy
+    current_user.update(roles_mask: 0)
+    redirect_to root_path
+  end
+
+  def index
+    @stores=Store.all
+  end
+
+  protected
+  #funzione che seatta un parametro
+  def store
+    @store=Store.find(params[:id])
+  end
+  #
+  def store_params
+    params.require(:store).permit(:name,:location)
+  end
+end
---
+++ Social_Flayer/spec/controllers/stores_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe StoresController, type: :controller do
+
+end
