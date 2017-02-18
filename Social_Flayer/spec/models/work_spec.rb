33ef8f3acae983789d3e6dbe70d91f90f1995148
Simone <simo.pelo94@gmail.com>
2017-02-18 19:01:25 +0100

    test su work.rb 	modified:   spec/models/work_spec.rb

--- Social_Flayer/spec/models/work_spec.rb
+++ Social_Flayer/spec/models/work_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Work, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @work_user = Work.new(store_id:1,user_id:1)
+    expect(@work_user).to_not eq(nil)
+  end
+
+  describe "validates" do
+    it "è ok" do
+      expect(@work_user).to be_valid
+    end
+    it "test unicità" do
+      @work_user.save
+      @work_user2 = Work.new(store_id:1,user_id:1)
+      expect(@work_user2).to_not be_valid
+    end
+
+  end
+  describe "associazioni" do
+     it "store" do
+        assc = described_class.reflect_on_association(:store)
+        expect(assc.macro).to eq :belongs_to
+      end
+      it "user" do
+        assc = described_class.reflect_on_association(:user)
+        expect(assc.macro).to eq :belongs_to
+      end
+  end
+
 end
