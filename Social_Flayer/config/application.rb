ca6705137b5b031434aee99ddcb3a588d7816766
Simone <simo.pelo94@gmail.com>
2017-02-28 12:18:32 +0100

    risolto not found roboto. 	deleted:    app/assets/javascripts/init.js 	deleted:    app/assets/javascripts/materialize.js 	deleted:    app/assets/javascripts/materialize.min.js 	renamed:    app/assets/stylesheets/application.css -> app/assets/stylesheets/application.css.scss 	deleted:    app/assets/stylesheets/materialize.css 	deleted:    app/assets/stylesheets/materialize.min.css 	modified:   config/application.rb

--- Social_Flayer/config/application.rb
+++ Social_Flayer/config/application.rb
@@ -6,10 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module SocialFlayer
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
